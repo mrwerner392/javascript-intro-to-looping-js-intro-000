d136996dbf1242856962dbd72de36d58d9b76fdd
Matt Werner <mrwerner392@gmail.com>
2019-04-06 17:55:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,3 +17,18 @@ function whileLoop(number) {
   }
   return 'done';
 }
+
+// logs a string at least one times
+function doWhileLoop(integer) {
+  let i = 0
+  
+  function incrementVariable() {
+    return ++i;
+  }
+  
+  do {
+    console.log('I run once regardless.');
+  } while (incrementVariable() < integer)
+
+    
+}
