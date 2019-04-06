d53bfa5d6fca2597d14a0a869e4e86470d8a5e8e
Matt Werner <mrwerner392@gmail.com>
2019-04-06 17:38:10 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+// add a string to an array 25 times with a for loop
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push('I am 1 strange loop.');
+    } else {
+      array.push('I am ${i} strange loops.');
+    }
+  }
+  return array;
+}
+
+
+
+
+
+
